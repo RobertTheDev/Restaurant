a796d80e0e97b2f5c9a53dd48367f086a9e41f85
RobertTheDev <robertthedev@gmail.com>
2024-03-26 15:24:25 +0000

    feat: imported and added navigator

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
-import ProductScreen from "./screens/Product";
+import Navigator from "./navigator";
 
 export default function App() {
     return (
         <SafeAreaProvider>
             <SafeAreaView style={{ flex: 1 }}>
                 <StatusBar style="auto" />
-                <ProductScreen />
+                <Navigator />
             </SafeAreaView>
         </SafeAreaProvider>
     );
