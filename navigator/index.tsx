d117ecf26bd369fec712f59fddfe6db766389493
RobertTheDev <robertthedev@gmail.com>
2024-03-26 17:00:02 +0000

    feat: installed and set up bottom tab navigator

--- navigator/index.tsx
+++ navigator/index.tsx
@@ -1,28 +1,106 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import {
+    BottomTabBarButtonProps,
+    createBottomTabNavigator
+} from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Pressable, Text } from "react-native";
 
-import Header from "../components/Header";
+import AccountScreen from "../screens/Account";
 import HomeScreen from "../screens/Home";
 import ProductScreen from "../screens/Product";
 
 const Stack = createNativeStackNavigator();
 
+const Tab = createBottomTabNavigator();
+
+function TabButton({
+    props,
+    title
+}: {
+    props: BottomTabBarButtonProps;
+    title: string;
+}) {
+    return (
+        <Pressable
+            {...props}
+            style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+                paddingVertical: 8
+            }}
+        >
+            {title === "Home" && (
+                <MaterialIcons name="home" size={24} color="black" />
+            )}
+            {title === "Menu" && (
+                <MaterialIcons name="restaurant-menu" size={24} color="black" />
+            )}
+            {title === "Orders" && (
+                <MaterialIcons name="list-alt" size={24} color="black" />
+            )}
+            {title === "Account" && (
+                <MaterialIcons name="settings" size={24} color="black" />
+            )}
+            <Text style={{ fontSize: 10, fontWeight: "600" }}>{title}</Text>
+        </Pressable>
+    );
+}
+
+function HomeTabs() {
+    return (
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen
+                name="HomeTabs"
+                component={HomeScreen}
+                options={{
+                    tabBarButton: (props) => (
+                        <TabButton props={props} title="Home" />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="MenuTabs"
+                component={HomeScreen}
+                options={{
+                    tabBarButton: (props) => (
+                        <TabButton props={props} title="Menu" />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="OrderTabs"
+                component={HomeScreen}
+                options={{
+                    tabBarButton: (props) => (
+                        <TabButton props={props} title="Orders" />
+                    )
+                }}
+            />
+            <Tab.Screen
+                name="AccountTabs"
+                component={AccountScreen}
+                options={{
+                    tabBarButton: (props) => (
+                        <TabButton props={props} title="Account" />
+                    )
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
 function Navigator() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="Home">
-                <Stack.Screen
-                    name="Home"
-                    component={HomeScreen}
-                    options={{
-                        header: () => <Header />
-                    }}
-                />
-                <Stack.Screen
-                    name="Product"
-                    component={ProductScreen}
-                    options={{ headerShown: false }}
-                />
+            <Stack.Navigator
+                initialRouteName="Home"
+                screenOptions={{ headerShown: false }}
+            >
+                <Stack.Screen name="Home" component={HomeTabs} />
+                <Stack.Screen name="Product" component={ProductScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
