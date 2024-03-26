3b17ad66e4ecfa6a968648cf95a4b2beaf62698e
RobertTheDev <robertthedev@gmail.com>
2024-03-26 10:06:11 +0000

    feat: installed and set up react native safe area context

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import RestaurantScreen from "./screens/RestaurantScreen";
 
 export default function App() {
     return (
-        <>
+        <SafeAreaView style={{ flex: 1 }}>
             <StatusBar style="auto" />
             <RestaurantScreen />
-        </>
+        </SafeAreaView>
     );
 }
