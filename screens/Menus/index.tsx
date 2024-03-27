460e9ef6ac8ef6adc6ee595999881b22bd070a0a
RobertTheDev <robertthedev@gmail.com>
2024-03-27 14:16:53 +0000

    feat: added list header component

--- screens/Menus/index.tsx
+++ screens/Menus/index.tsx
@@ -1,11 +1,21 @@
-import { FlatList, Pressable, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 
 import menusData from "../../data/menus";
 import IMenu from "../../interfaces/Menu";
 
 function MenuCard({ item }: { item: IMenu }) {
+    const navigation = useNavigation();
+
     return (
-        <Pressable>
+        <Pressable
+            style={{ alignItems: "center" }}
+            onPress={() => navigation.navigate("Menu", { slug: item.slug })}
+        >
+            <Image
+                style={{ width: 160, height: 160, borderRadius: 99 }}
+                source={{ uri: item.imageUrl }}
+            />
             <Text>{item.name}</Text>
         </Pressable>
     );
@@ -14,7 +24,22 @@ function MenuCard({ item }: { item: IMenu }) {
 export default function MenusScreen() {
     return (
         <FlatList
+            ListHeaderComponent={() => (
+                <View>
+                    <Text style={{ fontSize: 20, fontWeight: "500" }}>
+                        Menus
+                    </Text>
+                </View>
+            )}
+            numColumns={2}
             data={menusData}
+            contentContainerStyle={{
+                paddingVertical: 32,
+                gap: 32,
+                alignItems: "center"
+            }}
+            style={{ width: "100%" }}
+            columnWrapperStyle={{ gap: 32 }}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => {
                 return <MenuCard item={item} />;
