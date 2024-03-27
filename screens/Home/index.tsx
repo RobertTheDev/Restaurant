390f3d93ddcd1a664242aa5cfda205693b966e7f
RobertTheDev <robertthedev@gmail.com>
2024-03-27 14:16:27 +0000

    feat: added basket button

--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,17 +1,21 @@
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 
 import ProductCard from "../../components/ProductCard";
 import products from "../../data/products";
+import BasketButton from "../../components/BasketButton";
 
 export default function HomeScreen() {
     return (
-        <FlatList
-            data={products}
-            keyExtractor={(_product, index) => index.toString()}
-            contentContainerStyle={{ gap: 24, paddingVertical: 24 }}
-            renderItem={({ item }) => {
-                return <ProductCard item={item} />;
-            }}
-        />
+        <View style={{ flex: 1, width: "100%" }}>
+            <BasketButton />
+            <FlatList
+                data={products}
+                keyExtractor={(_product, index) => index.toString()}
+                contentContainerStyle={{ gap: 24, paddingVertical: 24 }}
+                renderItem={({ item }) => {
+                    return <ProductCard item={item} />;
+                }}
+            />
+        </View>
     );
 }
