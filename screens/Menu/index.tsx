0ecbd5e58f5265cd62c20a778ad0cc904445b3ee
RobertTheDev <robertthedev@gmail.com>
2024-03-27 14:16:43 +0000

    feat: added list header component

--- screens/Menu/index.tsx
+++ screens/Menu/index.tsx
@@ -1,12 +1,39 @@
-import { FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { FlatList, Image, Text, View } from "react-native";
 
 import ProductCard from "../../components/ProductCard";
 import menusData from "../../data/menus";
 
 export default function MenuScreen() {
+    const route = useRoute();
+
+    const { slug } = route.params;
+
+    const menu = menusData.find((menu) => menu.slug === slug);
+
     return (
         <FlatList
-            data={menusData.find((menu) => menu.slug === "pastas")?.products}
+            ListHeaderComponent={() => (
+                <View>
+                    <View
+                        style={{
+                            height: 100,
+                            width: 100,
+                            borderRadius: 80,
+                            overflow: "hidden"
+                        }}
+                    >
+                        <Image
+                            style={{ height: "100%", width: "100%" }}
+                            source={{ uri: menu?.imageUrl }}
+                        />
+                    </View>
+                    <Text style={{ fontSize: 20, fontWeight: "500" }}>
+                        {menu.name}
+                    </Text>
+                </View>
+            )}
+            data={menu?.products}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => {
                 return <ProductCard item={item} />;
