3ce1d767b641f06bbfb166ab7f850546840dedd8
RobertTheDev <robertthedev@gmail.com>
2024-03-27 14:17:12 +0000

    feat: created order address screen

---
+++ screens/OrderAddress/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function OrderAddress() {
+    return (
+        <View>
+            <Text>Order Address</Text>
+        </View>
+    );
+}
