6d34bf83b81d68c2148b095437cfd41e72a077f8
RobertTheDev <robertthedev@gmail.com>
2024-03-27 10:44:10 +0000

    feat: created edit account screen

---
+++ screens/EditAccount/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function EditAccountScreen() {
+    return (
+        <View>
+            <Text>Edit Account</Text>
+        </View>
+    );
+}
