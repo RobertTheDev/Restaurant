f4eef2dea1804167f533c3c0e4897d49937d8291
RobertTheDev <robertthedev@gmail.com>
2024-03-27 14:16:06 +0000

    feat: added styles

--- screens/AddAddress/index.tsx
+++ screens/AddAddress/index.tsx
@@ -4,15 +4,24 @@ import MapView from "react-native-maps";
 export default function AddAddressScreen() {
     return (
         <View style={{ padding: 16 }}>
-            <MapView
-                style={{ aspectRatio: "1/1", width: "100%" }}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
+            <View
+                style={{
+                    aspectRatio: "4/3",
+                    borderRadius: 16,
+                    overflow: "hidden",
+                    width: "100%"
                 }}
-            />
+            >
+                <MapView
+                    style={{ height: "100%", width: "100%" }}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    }}
+                />
+            </View>
         </View>
     );
 }
