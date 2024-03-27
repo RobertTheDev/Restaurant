d14162dbbc6bc3d4c9b3d8891335a810e0597228
RobertTheDev <robertthedev@gmail.com>
2024-03-27 14:10:43 +0000

    ci: deleted files

--- screens/SelectDeliveryTime/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function SelectDeliveryTimeScreen() {
-    return (
-        <View>
-            <Text>Select Delivery Time</Text>
-        </View>
-    );
-}
