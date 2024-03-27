28b047e7dd1698c3352976b87cd61b5f8ef7f938
RobertTheDev <robertthedev@gmail.com>
2024-03-27 10:44:36 +0000

    feat: created select delivery screen

---
+++ screens/SelectDeliveryTime/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SelectDeliveryTimeScreen() {
+    return (
+        <View>
+            <Text>Select Delivery Time</Text>
+        </View>
+    );
+}
