4f2298a21f8e455b4aeff92657b31f89fa8ac5ee
RobertTheDev <robertthedev@gmail.com>
2024-03-28 07:54:00 +0000

    feat: added hash blur image

--- screens/Menus/index.tsx
+++ screens/Menus/index.tsx
@@ -1,8 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { FlatList, Pressable, Text, View } from "react-native";
 
 import menusData from "../../data/menus";
 import IMenu from "../../interfaces/Menu";
+import blurHashImage from "../../lib/hashBlurImage";
 
 function MenuCard({ item }: { item: IMenu }) {
     const navigation = useNavigation();
@@ -18,13 +20,16 @@ function MenuCard({ item }: { item: IMenu }) {
         >
             <View style={{ paddingHorizontal: 16, marginBottom: 16 }}>
                 <Image
+                    source={{ uri: item.image.uri }}
+                    placeholder={blurHashImage}
+                    contentFit="cover"
+                    transition={1000}
                     style={{
                         aspectRatio: "1/1",
                         width: "100%",
                         borderRadius: 99,
                         overflow: "hidden"
                     }}
-                    source={{ uri: item.image }}
                 />
             </View>
             <Text style={{ fontSize: 16, fontWeight: "500" }}>{item.name}</Text>
