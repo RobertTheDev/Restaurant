6e27e2d85826de07edeab0e1329047cb8f42fef6
RobertTheDev <robertthedev@gmail.com>
2024-03-28 07:11:52 +0000

    feat: added account link icons

--- screens/Account/index.tsx
+++ screens/Account/index.tsx
@@ -1,5 +1,13 @@
+import { Feather, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
-import { Alert, FlatList, Pressable, Text, View } from "react-native";
+import {
+    Alert,
+    Dimensions,
+    FlatList,
+    Pressable,
+    Text,
+    View
+} from "react-native";
 
 import accountLinks from "./accountLinks";
 
@@ -8,24 +16,55 @@ export default function AccountScreen() {
 
     return (
         <FlatList
+            bounces={false}
+            contentContainerStyle={{
+                flex: 1,
+                alignItems: "center",
+                paddingVertical: 32,
+                gap: 16
+            }}
             ListHeaderComponent={() => (
                 <View>
-                    <Text>Sanjiv</Text>
+                    <Feather name="user" size={48} />
+                    <Text
+                        style={{
+                            marginBottom: 16,
+                            marginTop: 8,
+                            fontSize: 18,
+                            fontWeight: "500"
+                        }}
+                    >
+                        Sanjiv
+                    </Text>
                 </View>
             )}
             data={accountLinks}
             keyExtractor={(_link, index) => index.toString()}
             renderItem={({ item }) => (
                 <Pressable
+                    style={{
+                        backgroundColor: "white",
+                        width: Dimensions.get("window").width * 0.8,
+                        paddingVertical: 16,
+                        paddingHorizontal: 24,
+                        borderRadius: 8,
+                        overflow: "hidden",
+                        flexDirection: "row",
+                        gap: 24,
+                        alignItems: "center"
+                    }}
                     onPress={() => {
                         navigation.navigate(item.link);
                     }}
                 >
-                    <Text>{item.name}</Text>
+                    <MaterialCommunityIcons name={item.icon} size={24} />
+                    <Text style={{ fontSize: 16, fontWeight: "500" }}>
+                        {item.name}
+                    </Text>
                 </Pressable>
             )}
             ListFooterComponent={() => (
-                <View>
+                <View style={{ marginTop: 16 }}>
                     <Pressable
                         onPress={() => {
                             Alert.alert(
@@ -46,7 +85,12 @@ export default function AccountScreen() {
                             );
                         }}
                     >
-                        <Text style={{ textDecorationLine: "underline" }}>
+                        <Text
+                            style={{
+                                textDecorationLine: "underline",
+                                fontSize: 14
+                            }}
+                        >
                             Sign Out
                         </Text>
                     </Pressable>
