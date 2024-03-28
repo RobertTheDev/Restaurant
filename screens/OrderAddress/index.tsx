166ce7217d051f3a08fe9b9251255e440836cd23
RobertTheDev <robertthedev@gmail.com>
2024-03-28 12:17:39 +0000

    ci: deleted files

--- screens/OrderAddress/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function OrderAddress() {
-    return (
-        <View>
-            <Text>Order Address</Text>
-        </View>
-    );
-}
