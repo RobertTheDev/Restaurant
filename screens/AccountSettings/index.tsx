01c8f4e756651f8eaa3f3444660a0394384e9710
RobertTheDev <robertthedev@gmail.com>
2024-03-28 10:42:51 +0000

    feat: added change password input

--- screens/AccountSettings/index.tsx
+++ screens/AccountSettings/index.tsx
@@ -1,15 +1,44 @@
 import { useNavigation } from "@react-navigation/native";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 export default function AccountSettingsScreen() {
     const { goBack } = useNavigation();
 
     return (
         <View>
-            <Pressable onPress={goBack}>
-                <Text>Back</Text>
-            </Pressable>
-            <Text>Account Settings</Text>
+            <View>
+                <Pressable onPress={goBack}>
+                    <Text>Back</Text>
+                </Pressable>
+                <Text>Account Settings</Text>
+            </View>
+
+            <View>
+                <Text>Change Email</Text>
+                <Text>You can change your email here</Text>
+                <TextInput placeholder="Change Email" />
+                <Pressable>
+                    <Text>Change Email</Text>
+                </Pressable>
+            </View>
+            <View>
+                <Text>Change Password</Text>
+                <Text>You can change your password hre</Text>
+                <TextInput placeholder="Current Password" />
+                <TextInput placeholder="New Password" />
+                <Pressable>
+                    <Text>Change Password</Text>
+                </Pressable>
+            </View>
+            <View>
+                <Text>Close Account</Text>
+                <Text>You can close your account here</Text>
+                <TextInput placeholder="Type Delete" />
+
+                <Pressable>
+                    <Text>Close Account</Text>
+                </Pressable>
+            </View>
         </View>
     );
 }
