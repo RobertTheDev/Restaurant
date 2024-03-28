dd38ed2a9a0dd4d1b94d29d38d1650c979ed8e0a
RobertTheDev <robertthedev@gmail.com>
2024-03-28 12:18:28 +0000

    feat: added navigatio buttons

--- screens/ManagePaymentMethods/index.tsx
+++ screens/ManagePaymentMethods/index.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 import { Pressable, Text, View } from "react-native";
 
 export default function ManagePaymentMethods() {
+    const { navigate } = useNavigation();
+
     return (
         <View>
             <Text>Manage Payment Methods</Text>
             <Text>Your payment methods will appear here.</Text>
-            <Pressable>
+            <Pressable onPress={() => navigate("AddPaymentMethod")}>
                 <Text>Add A Payment Methods</Text>
             </Pressable>
 
-            <Pressable>
-                <Text>Update Payment Method Card</Text>
+            <Pressable onPress={() => navigate("UpdatePaymentMethod")}>
+                <Text>Visa ending in 5536</Text>
+                <Text>Exp date 06/24</Text>
+                <Pressable
+                    onPress={() => {
+                        console.log("Set as default");
+                    }}
+                >
+                    <Text>Set As Default</Text>
+                </Pressable>
+                <Pressable
+                    onPress={() => {
+                        console.log("Deleted");
+                    }}
+                >
+                    <MaterialCommunityIcons name="trash-can-outline" />
+                </Pressable>
             </Pressable>
         </View>
     );
