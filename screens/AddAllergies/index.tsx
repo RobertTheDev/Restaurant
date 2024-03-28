1f786435117734c7972491412792856785ca6d16
RobertTheDev <robertthedev@gmail.com>
2024-03-28 10:42:38 +0000

    feat: added haptics on select

--- screens/AddAllergies/index.tsx
+++ screens/AddAllergies/index.tsx
@@ -1,4 +1,6 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import * as Haptics from "expo-haptics";
 import { FlatList, Pressable, Text, View } from "react-native";
 
 import allergies from "../../data/allergies";
@@ -10,26 +12,46 @@ export default function AddAllergiesScreen() {
         <FlatList
             ListHeaderComponent={() => {
                 return (
-                    <View>
+                    <View
+                        style={{
+                            height: 60,
+                            width: "100%",
+                            backgroundColor: "white",
+                            paddingHorizontal: 16,
+                            alignItems: "center",
+                            flexDirection: "row"
+                        }}
+                    >
                         <Pressable onPress={goBack}>
-                            <Text>Back</Text>
+                            <MaterialCommunityIcons
+                                name="chevron-left"
+                                size={32}
+                            />
                         </Pressable>
+                        <Text>Add Allergies</Text>
                     </View>
                 );
             }}
             data={allergies}
+            stickyHeaderIndices={[0]}
             keyExtractor={(_allergy, index) => index.toString()}
             renderItem={({ item }) => (
-                <View
+                <Pressable
+                    onPress={() => {
+                        Haptics.impactAsync();
+                        console.log(item.icon);
+                    }}
                     style={{
                         flexDirection: "row",
                         alignItems: "center",
-                        gap: 8
+                        gap: 16,
+                        paddingHorizontal: 16,
+                        paddingVertical: 8
                     }}
                 >
                     <Text style={{ fontSize: 24 }}>{item.icon}</Text>
                     <Text style={{ fontSize: 16 }}>{item.value}</Text>
-                </View>
+                </Pressable>
             )}
         />
     );
